2ae78e4ddbfa33deaaaa9a93337239174f7b2aab
kkoouuya0730 <kouya.0730@icloud.com>
2025-11-01 22:19:28 +0900

    HonoのRPC機能を利用した型安全な通信のサンプル作成

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,19 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  "*",
+  cors({
+    origin: "*",
+  })
+);
 
-export default app
+const route = app.get("/hello", (c) => {
+  return c.json({ message: "Hello Hono!" });
+});
+
+export type AppType = typeof route;
+
+export default app;
