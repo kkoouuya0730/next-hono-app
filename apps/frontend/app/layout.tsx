94f9863577534247bc9c1968609dddfe736f480b
kkoouuya0730 <kouya.0730@icloud.com>
2025-12-24 00:05:36 +0900

    レイアウト調整

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./Provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Header } from "@/components/Header/Header";
+import { Footer } from "@/components/Footer/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +15,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Provider>{children}</Provider>
+    <html lang="ja">
+      <body>
+        <Provider>
+          <Header />
+          <main className="page-container">{children}</main>
+          <Footer />
+        </Provider>
       </body>
     </html>
   );
