c8a7742bd4dc70661681d6dd8831a531b8b85b05
kkoouuya0730 <kouya.0730@icloud.com>
2026-01-13 22:46:10 +0900

    ミドルウェアでトークンチェック

--- apps/frontend/app/posts/layout.tsx
+++ apps/frontend/app/posts/layout.tsx
@@ -1,19 +1,5 @@
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
-import { useAuthContext } from "../providers/AuthProvider";
-
 export default function PostsLayout({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-  const pathname = usePathname();
-  const { isAuthenticated, isLoading } = useAuthContext();
-
-  if (isLoading) return <p>認証確認中...</p>;
-  if (!isAuthenticated) {
-    const redirect = encodeURIComponent(pathname);
-    router.replace(`/auth/signin?redirect=${redirect}`);
-    return null;
-  }
-
   return <>{children}</>;
 }
---
+++ apps/frontend/proxy.ts
@@ -0,0 +1,21 @@
+import { type NextRequest, NextResponse } from "next/server";
+
+export function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const token = request.cookies.get("idToken")?.value;
+  if (!token) {
+    // リダイレクト先のURLを、現在のリクエストURLを基準に生成
+    const signinUrl = new URL("/auth/signin", request.url);
+    // リダイレクト先での処理の後に元の場所に戻すためにredirectパラメータを設定
+    signinUrl.searchParams.set("redirect", pathname);
+
+    return NextResponse.redirect(signinUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/posts/:path*"],
+};
